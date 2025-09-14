aa862caa10b4f8f0ee32e51bbe4c3d83d862777a
Kairgeldin Dmitry <dmitriy.k@densertech.com>
2025-09-14 17:50:51 +0500

    fix: middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={nunito.variable}>
+      <body className={nunito.variable} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,15 @@
+
 import createMiddleware from 'next-intl/middleware';
 
-// Поддерживаемые локали
 export const locales = ['ru', 'kz'] as const;
 export const defaultLocale = 'ru' as const;
 
 export default createMiddleware({
-  // Список поддерживаемых локалей
   locales,
-  
-  // Локаль по умолчанию
-  defaultLocale
+  defaultLocale,
+  localeDetection: true,
 });
+
+export const config = {
+  matcher: ['/', '/(ru|kz)/:path*'],
+};

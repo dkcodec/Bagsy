7886947e02e2bbab8ad727628f980c97501b5647
Kairgeldin Dmitry <dmitriy.k@densertech.com>
2026-01-19 09:26:57 +0500

    fix: ssr

--- src/features/appointment/ui/appointment-header.tsx
+++ src/features/appointment/ui/appointment-header.tsx
@@ -6,7 +6,7 @@ import { getLocale } from "next-intl/server";
 
 export async function AppointmentHeader() {
   const locale = await getLocale();
-  
+
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white/40 dark:bg-background/40 backdrop-blur-md border-b border-backgroung dark:border-background">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">

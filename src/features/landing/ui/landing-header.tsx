e78439a87ba275dcf40b4c95ed951640377cb421
Kairgeldin Dmitry <dmitriy.k@densertech.com>
2026-01-25 15:15:09 +0500

    format

--- src/features/landing/ui/landing-header.tsx
+++ src/features/landing/ui/landing-header.tsx
@@ -19,9 +19,7 @@ export function LandingHeader() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href={`/${locale}`}
-              hrefLang={locale}
-            >
+            <Link href={`/${locale}`} hrefLang={locale}>
               <LandingLogo className="h-8 w-auto" />
             </Link>
           </div>
